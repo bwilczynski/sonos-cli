1442088962f4a9afff9eab527cbb6a1a7cb3d91d
Bartlomiej Wilczynski <me@bwilczynski.com>
2018-11-25 15:35:29 +0100

    Open webbrowser and run Flask server to wait for authorization callback

--- app.py
+++ app.py
@@ -1,6 +1,18 @@
-from flask import Flask
+from flask import Flask, request
+from http import HTTPStatus
+import webbrowser
+
+from config import AUTH_URI
+
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello World!"
+
+@app.route('/', methods=['POST'])
+def callback():
+    print(request.query_string)
+    return ('', HTTPStatus.NO_CONTENT)
+
+
+if __name__ == '__main__':
+    webbrowser.open_new(AUTH_URI)
+    app.run()
---
+++ config.py
@@ -0,0 +1,4 @@
+CLIENT_ID = "***REMOVED***"
+REDIRECT_URI = "https://0c07cec7.ngrok.io"
+
+AUTH_URI = f"https://api.sonos.com/login/v3/oauth?client_id={CLIENT_ID}&response_type=code&state=1&scope=playback-control-all&redirect_uri={REDIRECT_URI}"
