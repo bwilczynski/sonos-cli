5ac240614716e8933fe71a271428aa063b7fe58a
Bartlomiej Wilczynski <me@bwilczynski.com>
2018-11-25 22:06:20 +0100

    Obtain access_token from Sonos API

--- app.py
+++ app.py
@@ -1,18 +1,30 @@
 from flask import Flask, request, render_template
 from http import HTTPStatus
 import webbrowser
+import requests
+from requests.auth import HTTPBasicAuth
 
-from config import AUTH_CODE_URI, PORT_NO, SONOS_ACCOUNT_URI
+from config import AUTH_CODE_URL, PORT_NO, SONOS_ACCOUNT_URL, CREATE_TOKEN_URL, CLIENT_ID, CLIENT_SECRET, CREATE_AUTH_CODE_REDIRECT_URL
 
 app = Flask(__name__)
 
 
+def create_token(code):
+    data = dict(grant_type='authorization_code',
+                code=code, redirect_uri=CREATE_AUTH_CODE_REDIRECT_URL)
+    response = requests.post(CREATE_TOKEN_URL, data=data,
+                             auth=HTTPBasicAuth(CLIENT_ID, CLIENT_SECRET))
+    print(response.json())
+
+
 @app.route('/authorized', methods=['GET'])
 def authorized():
-    print(request.query_string)
-    return render_template('authorized.html', redirect_url=SONOS_ACCOUNT_URI)
+    code = request.args['code']
+    print(code)
+    create_token(code)
+    return render_template('authorized.html', redirect_url=SONOS_ACCOUNT_URL)
 
 
 if __name__ == '__main__':
-    webbrowser.open_new(AUTH_CODE_URI)
+    webbrowser.open_new(AUTH_CODE_URL)
     app.run(port=PORT_NO)
