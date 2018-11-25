f37ece5a436aaef4b3c08425f7b884f0c6bc09b3
Bartlomiej Wilczynski <me@bwilczynski.com>
2018-11-25 19:06:54 +0100

    Connect to Sonos API using AWS lambda as return_uri

--- app.py
+++ app.py
@@ -2,17 +2,17 @@ from flask import Flask, request
 from http import HTTPStatus
 import webbrowser
 
-from config import AUTH_URI
+from config import AUTH_CODE_URI, PORT_NO
 
 app = Flask(__name__)
 
 
-@app.route('/', methods=['POST'])
+@app.route('/authorized', methods=['GET'])
 def callback():
     print(request.query_string)
     return ('', HTTPStatus.NO_CONTENT)
 
 
 if __name__ == '__main__':
-    webbrowser.open_new(AUTH_URI)
-    app.run()
+    webbrowser.open_new(AUTH_CODE_URI)
+    app.run(port=PORT_NO)
--- config.py
+++ config.py
@@ -1,4 +1,6 @@
-CLIENT_ID = "***REMOVED***"
-REDIRECT_URI = "https://0c07cec7.ngrok.io"
+# from urllib import parse
 
-AUTH_URI = f"https://api.sonos.com/login/v3/oauth?client_id={CLIENT_ID}&response_type=code&state=1&scope=playback-control-all&redirect_uri={REDIRECT_URI}"
+PORT_NO = 5000
+CLIENT_ID = '***REMOVED***'
+CREATE_AUTH_CODE_REDIRECT_URI = f'https://haa5mxcg4k.execute-api.eu-west-1.amazonaws.com/default/authorized'
+AUTH_CODE_URI = f'https://api.sonos.com/login/v3/oauth?client_id={CLIENT_ID}&response_type=code&state=1&scope=playback-control-all&redirect_uri={CREATE_AUTH_CODE_REDIRECT_URI}'
