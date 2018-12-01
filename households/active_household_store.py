817d7249f2af30395f007532f40f1e1e4b964e16
Bartlomiej Wilczynski <me@bwilczynski.com>
2018-12-01 21:41:58 +0100

    Set active household

---
+++ households/active_household_store.py
@@ -0,0 +1,11 @@
+import local_store
+
+ACTIVE_HOUSEHOLD_FILE = 'household.json'
+
+
+def save_active_household(household):
+    local_store.save(ACTIVE_HOUSEHOLD_FILE, household)
+
+
+def get_active_household():
+    return local_store.load(ACTIVE_HOUSEHOLD_FILE)
--- households/commands.py
+++ households/commands.py
@@ -1,10 +1,9 @@
-import json
-
 import click
 from requests_oauthlib import OAuth2Session
 
 from auth import get_access_token
 from config import CLIENT_ID, SONOS_CONTROL_API_BASE_URL
+from households import active_household_store
 
 
 @click.group()
@@ -17,4 +16,16 @@ def list_households():
     token = get_access_token()
     client = OAuth2Session(CLIENT_ID, token=token)
     result = client.get(f'{SONOS_CONTROL_API_BASE_URL}/households').json()
-    print(json.dumps(result))
+    print_households(result)
+
+
+@households.command('use')
+@click.argument('household_id')
+def use_household(household_id):
+    active_household_store.save_active_household(household_id)
+
+
+def print_households(data):
+    res = [household["id"] for household in data["households"]]
+    for h in res:
+        click.echo(h)
