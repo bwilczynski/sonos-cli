7464a860915f21a61cc32e5a8fa588804644f8aa
Bartlomiej Wilczynski <me@bwilczynski.com>
2018-12-01 22:40:14 +0100

    Format groups list output as table

--- groups/commands.py
+++ groups/commands.py
@@ -2,6 +2,7 @@ import json
 
 import click
 from requests_oauthlib import OAuth2Session
+from tabulate import tabulate
 
 from auth import get_access_token
 from config import CLIENT_ID, SONOS_CONTROL_API_BASE_URL
@@ -14,15 +15,22 @@ def groups():
 
 
 @groups.command('list')
-def list_groups():
+@click.option('--output', '-o', default='table')
+def list_groups(output):
     household_id = get_active_household()
 
     token = get_access_token()
     client = OAuth2Session(CLIENT_ID, token=token)
     result = client.get(f'{SONOS_CONTROL_API_BASE_URL}/households/{household_id}/groups').json()
-    print_groups(result)
+    print_groups_json(result) if output == 'json' else print_groups_table(result)
 
 
-def print_groups(data):
+def print_groups_json(data):
     prettified = json.dumps(data, indent=2, sort_keys=True)
     click.echo(prettified)
+
+
+def print_groups_table(data):
+    headers = ['id', 'name', 'playbackState']
+    rows = [[group[header] for header in headers] for group in data['groups']]
+    click.echo(tabulate(rows, headers=headers))
--- setup.py
+++ setup.py
@@ -7,7 +7,8 @@ setup(
     install_requires=[
         'click',
         'requests',
-        'requests_oauthlib'
+        'requests_oauthlib',
+        'tabulate'
     ],
     entry_points='''
         [console_scripts]
