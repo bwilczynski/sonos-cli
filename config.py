b86999a69436c9e1bc6b53518650267bdddbbfa0
Bartlomiej Wilczynski <me@bwilczynski.com>
2018-12-09 18:30:55 +0100

    Set CLIENT_ID & CLIENT_SECRET from getenv

--- config.py
+++ config.py
@@ -1,5 +1,7 @@
-CLIENT_ID = '***REMOVED***'
-CLIENT_SECRET = '***REMOVED***'
+import os
+
+CLIENT_ID = os.getenv('CLIENT_ID')
+CLIENT_SECRET = os.getenv('CLIENT_SECRET')
 
 SONOS_AUTH_API_BASE_URL = 'https://api.sonos.com'
 SONOS_CONTROL_API_BASE_URL = 'https://api.ws.sonos.com/control/api/v1'
