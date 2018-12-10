100c6ae74feba6b80c247766d3034fc48370c1ca
Bartlomiej Wilczynski <me@bwilczynski.com>
2018-12-10 22:51:22 +0100

    Handle access denied in login command

--- commands/login.py
+++ commands/login.py
@@ -11,24 +11,22 @@ from config.creds_store import save_access_token
 from settings import CLIENT_REDIRECT_PORT_NO
 
 
+class AuthenticationError(Exception):
+    pass
+
+
 def _get_access_token(code):
     return auth.get_access_token(code)
 
 
 def _get_authorization_code(state):
-    class AuthenticationError(Exception):
-        pass
-
     class ClientRedirectServer(HTTPServer):
         query_params = {}
 
     class ClientRedirectHandler(BaseHTTPRequestHandler):
         def do_GET(self):
-            print(self.headers.get('Referer'))
             query_string = self.path.split('?', 1)[-1]
             self.server.query_params = parse_qs(query_string)
-
-            print(self.server.query_params)
 
             self.send_response(HTTPStatus.OK)
             self.send_header('Content-type', 'text/html')
@@ -39,22 +37,33 @@ def _get_authorization_code(state):
             with open(html_file, 'rb') as html_view:
                 self.wfile.write(html_view.read())
 
+        def log_message(self, format, *args):
+            pass
+
     server = ClientRedirectServer(('', CLIENT_REDIRECT_PORT_NO), ClientRedirectHandler)
     while True:
         server.handle_request()
-        if 'code' and 'state' in server.query_params:
-            state_param = server.query_params['state'][0]
+        if 'code' in server.query_params:
             code_param = server.query_params['code'][0]
+            state_param = server.query_params['state'][0] if 'state' in server.query_params else None
+
             if state == state_param:
                 return code_param
             else:
                 raise AuthenticationError()
+        if 'error' in server.query_params:
+            raise AuthenticationError()
 
 
 @click.command()
 def login():
     url, state = auth.login()
     webbrowser.open_new(url)
-    code = _get_authorization_code(state)
-    data = _get_access_token(code)
-    save_access_token(data)
+    try:
+        code = _get_authorization_code(state)
+        data = _get_access_token(code)
+        save_access_token(data)
+        click.echo('Login successful.')
+    except AuthenticationError:
+        click.echo('Access was denied!')
+        exit(1)
--- decorators.py
+++ decorators.py
@@ -54,7 +54,7 @@ def login_required(func):
     @functools.wraps(func)
     def wrapper_login_required(*args, **kwargs):
         if creds_store.get_access_token() is None:
-            click.echo('Not logged in! Run `sonos-cli login` first.')
+            click.echo('Not logged in! Run `sonos login` first.')
             return None
         return func(*args, **kwargs)
 
