82620a772eadcf2b1f2dab92d251b467dfe48a78
bwilczynski <me@bwilczynski.com>
2020-10-06 22:04:36 +0200

    Add Github actions (#15)
    * Add Github actions
    
    * Use both Python 3.7 & 3.8 for nox session
    
    * Do not set arbitrary Python version for nox session
    
    * Run nox on different Python versions
    
    * Add deploy workflow
    
    * Remove azure-pipelines

--- noxfile.py
+++ noxfile.py
@@ -4,17 +4,15 @@ nox.options.sessions = ["lint"]
 
 locations = "sonos", "noxfile.py"
 
-DEFAULT_PYTHON = "3.7"
 
-
-@nox.session(python=DEFAULT_PYTHON)
+@nox.session
 def lint(session):
     args = session.posargs or locations
     session.install("flake8", "flake8-black")
     session.run("flake8", *args)
 
 
-@nox.session(python=DEFAULT_PYTHON)
+@nox.session
 def black(session):
     args = session.posargs or locations
     session.install("black")
