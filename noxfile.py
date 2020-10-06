5e8beb8c4e9925e87909cbec8daf76e0ba22e915
Bartlomiej Wilczynski <me@bwilczynski.com>
2020-10-06 21:21:29 +0200

    Do not set arbitrary Python version for nox session

--- noxfile.py
+++ noxfile.py
@@ -4,17 +4,15 @@ nox.options.sessions = ["lint"]
 
 locations = "sonos", "noxfile.py"
 
-PYTHON_INTERPRETERS = ["3.7", "3.8"]
 
-
-@nox.session(python=PYTHON_INTERPRETERS)
+@nox.session
 def lint(session):
     args = session.posargs or locations
     session.install("flake8", "flake8-black")
     session.run("flake8", *args)
 
 
-@nox.session(python=PYTHON_INTERPRETERS)
+@nox.session
 def black(session):
     args = session.posargs or locations
     session.install("black")
